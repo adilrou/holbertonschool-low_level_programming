3f75585433758d31b4de90b38c568e7439871b68
adilrou <10658@holbertonstudents.com>
2025-02-26 18:55:46 +0000

    O-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Print the string "_putchar" character by character */
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');  /* Print a new line */
+
+	return (0);
+}
+
